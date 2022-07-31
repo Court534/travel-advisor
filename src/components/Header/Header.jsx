66b503c5a0609b9f9b2238563e8ba7a46e4acd08
Courtney Stow <courtney-stow@hotmail.co.uk>
2022-07-31 12:17:05 +0100

    Currently having issues rendering the webpage. Quick commit to make sure i don't lose anything

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,8 +2,12 @@ import React from 'react';
 import { Autocomplete } from '@react-google-maps/api';
 import { AppBar, Toolbar, Typography, InputBase, Box } from '@material-ui/core';
 import SearchIcon from '@material-ui/icons/Search'
+import useStyles from './HeaderStyles.js'
 
 const Header = () => {
+  
+  const classes = useStyles();
+
   return (
     <AppBar position='static'>
         <Toolbar className={classes.toolbar}>
@@ -14,14 +18,14 @@ const Header = () => {
           <Typography variant='h6' className={classes.title}>
             Explore New Places
           </Typography>
-          <Autocomplete>
+          {/* <Autocomplete> */}
             <div className={classes.search}>
               <div className={classes.searchIcon}>
                 <SearchIcon />
               </div>
               <InputBase placeholder='Search ...' classes={{ root: classes.inputRoot, input: classes.inputInput }}/>
             </div>
-          </Autocomplete>
+          {/* </Autocomplete> */}
           </Box>
         </Toolbar>
     </AppBar>
