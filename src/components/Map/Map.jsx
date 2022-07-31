297bfd69fd7d5cdcf2902fce3163daae6f8576c7
Courtney Stow <courtney-stow@hotmail.co.uk>
2022-07-31 17:09:03 +0100

    Linked an API key for google maps and is up and running

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -15,7 +15,7 @@ const Map = () => {
   return (
     <div className={classes.mapContainer}>
       <GoogleMapReact 
-        bootstrapURLKeys={{ key: '' }}
+        bootstrapURLKeys={{ key:'AIzaSyD5cP5aP2o5kr4ahsEMX_ef6hXRaKztOn8' }}
         defaultCenter={coordinates}
         center={coordinates}
         defaultZoom={14}
