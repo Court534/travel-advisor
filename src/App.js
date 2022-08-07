8ff7ba77ba5a4ad8e0c5a5d82984855dfaca2f9f
Courtney Stow <courtney-stow@hotmail.co.uk>
2022-08-07 17:21:51 +0100

    You can now selected the rating and it will only show the chosen rating that you have selected

--- src/App.js
+++ src/App.js
@@ -10,6 +10,9 @@ import { getPlaceData } from './API';
 
 const App = () => {
   const [places, setPlaces] = useState([]);
+  
+  const [filteredPlaces, setFilteredPlaces] = useState([]);
+  
   const [childClicked, setChildClicked] = useState(null);
 
   const [coordinates, setCoordinates] = useState({});
@@ -26,10 +29,17 @@ const App = () => {
   }, []);
 
   useEffect(() => {
+  const filteredPlaces = places.filter((place) => place.rating > rating);
+
+  setFilteredPlaces(filteredPlaces);
+  }, [rating])
+
+  useEffect(() => {
     setIsLoading(true);
     getPlaceData(type, bounds.sw, bounds.ne)
       .then((data) => {
         setPlaces(data);
+        setFilteredPlaces([]);
         setIsLoading(false);
       })
   }, [type, coordinates, bounds]);
@@ -41,7 +51,7 @@ const App = () => {
       <Grid container spacing={3} style={{ width: '100%' }}>
         <Grid item xs={12} md={4}>
           <List 
-            places={places}
+            places={filteredPlaces.length ? filteredPlaces : places}
             childClicked={childClicked}
             isLoading={isLoading}
             type={type}
@@ -55,7 +65,7 @@ const App = () => {
             setCoordinates={setCoordinates}
             setBounds={setBounds}
             coordinates={coordinates}
-            places={places}
+            places={filteredPlaces.length ? filteredPlaces : places}
             setChildClicked={setChildClicked}
           />
         </Grid>
